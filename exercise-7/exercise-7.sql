73b213bb3f8c5fb04404d173359f9a0af68dd92c
EmilyDSarani <emily.sarani@gmail.com>
2022-02-02 16:43:23 -0600

    7 done

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -2,11 +2,26 @@
 -- similar to exercise 6
 
 --so...we want just the sum?
-SELECT 
-    SUM(length)
---and...we will start at inventory    
-FROM inventory
---just go to film
-INNER JOIN film
+SELECT
+customer.email,
+RANK() OVER (ORDER BY SUM(film.length) DESC)
+--start a customer
+FROM film
+--go through inventory
+INNER JOIN inventory
 --find the common ground
 ON inventory.film_id = film.film_id
+--go to rental
+INNER JOIN rental
+--find the common ground
+ON rental.inventory_id = inventory.inventory_id
+--go through customer
+INNER JOIN customer
+--find the common ground
+ON rental.customer_id = customer.customer_id
+--group by the email
+GROUP BY
+customer.email
+--order by the rank asc
+ORDER BY
+ RANK ASC;
