9e19d643af1a7df86bae504db15a16f13d1004c2
EmilyDSarani <emily.sarani@gmail.com>
2022-02-02 16:15:28 -0600

    smh, 6 done

--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -1,3 +1,29 @@
 -- customer ranking by total amount spent
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
+
+--we want the customer...so maybe the id
+--the sum of the amount
+--rank-- this is the window thing Dan talked about
+
+--we want the email and the rank, which is a window agg
+SELECT
+customer.email,
+RANK() OVER (ORDER BY SUM(payment.amount) DESC)
+--start a customer
+FROM customer
+--GO THROUGH RENTAL SMH
+INNER JOIN rental
+--find the common ground
+ON rental.customer_id = customer.customer_id
+--go to payment
+INNER JOIN payment
+--find the common ground
+ON rental.rental_id = payment.rental_id
+--group by the email
+GROUP BY
+customer.email
+--order by the rank asc
+ORDER BY
+ RANK ASC;
+
--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,12 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+
+--so...we want just the sum?
+SELECT 
+    SUM(length)
+--and...we will start at inventory    
+FROM inventory
+--just go to film
+INNER JOIN film
+--find the common ground
+ON inventory.film_id = film.film_id
